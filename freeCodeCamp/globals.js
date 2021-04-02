acc0e280e8caff2e9b6e7fa073b3c3424fb31d7c
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-04-02 19:59:38 -0300

    freecodecamp

---
+++ freeCodeCamp/globals.js
@@ -0,0 +1,10 @@
+const dirname = __dirname
+const filename = __filename
+const req = require
+const modulee = module
+const processs = process
+console.log('dirname', dirname)
+console.log('filename', filename)
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
---
+++ freeCodeCamp/modules.js
@@ -0,0 +1,6 @@
+const sayHi = require('./utils')
+const { john, peter } = require('./names')
+
+sayHi('Luiz')
+sayHi(john)
+sayHi(peter)
